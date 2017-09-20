203ceca3149ab270244c20516cc8aa9d9c74a62f
BoggsParty <julia.kuzel@gmail.com>
2017-09-19 20:24:18 -0500

    migrations

--- flat_pages/views.py
+++ flat_pages/views.py
@@ -17,8 +17,9 @@ def calendar(request):
     
 @login_required
 def flat_page(request, slug):
+    pages = get_list_or_404(Flat_Page)
     content = get_object_or_404(Flat_Page, slug=slug)
     sports_nav = get_list_or_404(Sport)
-    return render(request, 'flat_pages/flat_page.html',{'sports_nav':sports_nav,'content':content,} )
+    return render(request, 'flat_pages/flat_page.html',{'sports_nav':sports_nav,'content':content,'pages':pages,} )
 
 
